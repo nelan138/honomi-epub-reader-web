bd6d105c6acc5719a10dd45d94c8c66978875df0
FoiBem2131 <trunghuyh2@gmail.com>
2026-09-21 13:26:04 +0700

    Testing cloudflare

--- src/views/LibraryView.vue
+++ src/views/LibraryView.vue
@@ -192,6 +192,8 @@ const router = useRouter();
       @import-files="handleImportingBooks"
    />
 
+   <div>test, if this is working</div>
+
    <BookShelf
       v-for="shelf in shelfStore.shelves"
       :key="shelf.id"
