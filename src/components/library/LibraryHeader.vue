ad8da4b25777ceb6cfbf40f2225796f9adfdaa31
nelan138 <contact.nelanhuynh@gmail.com>
2026-09-23 13:34:46 +0700

    moved: global css variables to component

--- src/components/library/LibraryHeader.vue
+++ src/components/library/LibraryHeader.vue
@@ -15,11 +15,11 @@ function onFileChange(event: Event) {
 
 <template>
    <header
-      class="hover:border-b-highlight bg-surface border-b-stroke/50 sticky top-0 z-50 mx-auto flex w-full items-center justify-between border-b-2 px-4 py-2 md:px-16 xl:px-32 2xl:px-64"
+      class="sticky top-0 z-50 mx-auto flex w-full items-center justify-between border-b border-(--library-header-border) bg-(--library-header-bg) px-4 py-2 md:px-16 xl:px-32 2xl:px-64"
    >
-      <ul class="text-ink/70 flex gap-6">
+      <ul class="flex gap-6">
          <li>
-            <label class="hover:text-highlight cursor-pointer">
+            <label class="cursor-pointer">
                <i class="fa-solid fa-upload scale-[110%]"></i>
                <input @change="onFileChange" class="hidden" type="file" accept=".epub,application/epub+zip" multiple />
             </label>
@@ -29,7 +29,7 @@ function onFileChange(event: Event) {
                href="https://github.com/nelan138/honomi-epub-reader-web"
                target="_blank"
                rel="noopener noreferrer"
-               class="hover:text-highlight"
+               class=""
             >
                <i class="fa-brands fa-github scale-[110%]"></i>
             </a>
@@ -38,20 +38,12 @@ function onFileChange(event: Event) {
 
       <ul class="text-ink/70 flex gap-6">
          <li>
-            <button
-               @click="emit('addShelf')"
-               type="button"
-               class="hover:text-highlight hover:cursor-pointer"
-            >
+            <button @click="emit('addShelf')" type="button" class="hover:cursor-pointer">
                <i class="fa-solid fa-folder-plus scale-[110%]"></i>
             </button>
          </li>
          <li>
-            <button
-               @click="emit('toggleTheme')"
-               type="button"
-               class="hover:text-highlight hover:cursor-pointer"
-            >
+            <button @click="emit('toggleTheme')" type="button" class="hover:cursor-pointer">
                <i class="fa-solid fa-circle-half-stroke scale-[110%]"></i>
             </button>
          </li>
@@ -59,4 +51,13 @@ function onFileChange(event: Event) {
    </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+:global(html) {
+   --library-header-bg: #f6f2e6;
+   --library-header-border: var(--tertiary);
+}
+:global(html.dark) {
+   --library-header-bg: #15181a;
+   --library-header-border: var(--tertiary);
+}
+</style>
