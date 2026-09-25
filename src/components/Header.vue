4589cc2588d02c540b261b1e6c54688de06c8dca
nelan138 <contact.nelanhuynh@gmail.com>
2026-09-25 11:36:35 +0700

    fixed: header positioning

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 
 <template>
    <header
-      class="mx-auto flex w-full items-center justify-between border-b-2 border-(--header-border) bg-(--header-bg) px-4 py-2 md:px-16 xl:px-32 2xl:px-64"
+      class="sticky top-0 z-100 mx-auto flex w-full items-center justify-between border-b-2 border-(--header-border) bg-(--header-bg) px-4 py-2 md:px-16 xl:px-32 2xl:px-64"
    >
       <slot>
          <!-- ! Buttons / Items here -->
--- src/views/ReaderView.vue
+++ src/views/ReaderView.vue
@@ -214,7 +214,7 @@ const openToc = ref(false);
 </script>
 
 <template>
-   <Header class="fixed z-100" v-if="openHeader">
+   <Header v-if="openHeader">
       <ul class="flex gap-6">
          <li>
             <button @click="router.push('/')" type="button" class="hover:cursor-pointer">
